6526a89d4fbf0b3c3cf14cbaf38f9e0b98538db0
Vignesh Pai <vigneshpai2003@gmail.com>
2024-03-24 17:27:01 +0530

    added stable pkgs

--- flake.nix
+++ flake.nix
@@ -4,25 +4,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.vignesh-inspiron = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/inspiron
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: {
+    nixosConfigurations.vignesh-inspiron =
+      let
+        system = "x86_64-linux";
+      in
+      nixpkgs.lib.nixosSystem {
+        system = system;
+        specialArgs = { pkgs-stable = nixpkgs-stable.legacyPackages.${system}; };
+        modules = [
+          ./hosts/inspiron
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.vignesh = import ./home;
-        }
-      ];
-    };
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.vignesh = import ./home;
+          }
+        ];
+      };
   };
 }
