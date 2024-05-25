0d8a2a59483181e87741fd8ed8374c4da1e9ea15
Vignesh Pai <57759856+vigneshpai2003@users.noreply.github.com>
2024-05-25 23:18:14 +0530

    nautilus open terminal dconf settings

--- home-manager/gnome/tilix.nix
+++ home-manager/gnome/tilix.nix
@@ -4,10 +4,16 @@
     tilix # terminal
   ];
 
-  dconf.settings."com/gexperts/Tilix" = {
-    app-title = "\${sessionName}";
-    terminal-title-style = "small";
-    use-tabs = true;
-    warn-vte-config-issue = false;
+  dconf.settings = {
+    "com/gexperts/Tilix" = {
+      app-title = "\${sessionName}";
+      terminal-title-style = "small";
+      use-tabs = true;
+      warn-vte-config-issue = false;
+    };
+
+    "com/github/stunkymonkey/nautilus-open-any-terminal" = {
+      terminal = "tilix";
+    };
   };
 }
