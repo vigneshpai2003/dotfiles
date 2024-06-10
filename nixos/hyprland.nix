c9b73d3537a03ca5aeb9c7fd55891b178e235156
Vignesh Pai <57759856+vigneshpai2003@users.noreply.github.com>
2024-06-10 22:30:35 +0530

    added config files

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 {
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
   programs.hyprland = {
     enable = true;
@@ -17,23 +17,15 @@
     hyprpaper
     polkit_gnome
     dunst
+    libnotify
+    (pkgs.writeShellScriptBin "polkit-gnome" ''
+      ...
+      ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
+      ...
+    '')
   ];
 
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
+  security.polkit.enable = true;
 
   # for electron/chromium apps to run wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
