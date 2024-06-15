568641c1e9e97915ae22b67ee8ef4fd024f7073f
Vignesh Pai <57759856+vigneshpai2003@users.noreply.github.com>
2024-06-16 01:50:08 +0530

    hyprlock fingerprint and password

--- nixos/system/fingerprint.nix
+++ nixos/system/fingerprint.nix
@@ -30,4 +30,22 @@
       session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
     '';
   };
+
+  security.pam.services.hyprlock = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      # Account management.
+      account required pam_unix.so # unix (order 10900)
+
+      # Authentication management.
+      auth sufficient pam_unix.so likeauth try_first_pass # unix (order 11500)
+      auth required pam_deny.so # deny (order 12300)
+
+      # Password management.
+      password sufficient pam_unix.so nullok yescrypt # unix (order 10200)
+
+      # Session management.
+      session required pam_env.so conffile=/etc/pam/environment readenv=0 # env (order 10100)
+      session required pam_unix.so # unix (order 10200)
+    '';
+  };
 }
