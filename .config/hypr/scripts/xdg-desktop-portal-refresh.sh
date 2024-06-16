54063e489af5ecd3a826f8ec35f5a8a1c3fca429
Vignesh Pai <57759856+vigneshpai2003@users.noreply.github.com>
2024-06-16 21:36:18 +0530

    better locking logs added starting xdg desktop portal to super R

---
+++ .config/hypr/scripts/xdg-desktop-portal-refresh.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if systemctl --user status xdg-desktop-portal-hyprland.service systemctl | grep "active (running)"; then
+    systemd-cat -t "vignesh@xdg-desktop-portal" echo "xdg-desktop-portal-hyprland is active"
+else
+    systemd-cat -t "vignesh@xdg-desktop-portal" echo "xdg-desktop-portal-hyprland is inactive, starting it"
+    systemctl --user start xdg-desktop-portal-hyprland.service
+fi
