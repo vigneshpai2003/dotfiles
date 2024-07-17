71dd9bd52670f41eaa25643d6ce6e8ffd0b86268
Vignesh Pai <57759856+vigneshpai2003@users.noreply.github.com>
2024-07-17 16:19:14 +0530

    updated riseup lock added steam

--- hosts/inspiron/configuration.nix
+++ hosts/inspiron/configuration.nix
@@ -109,12 +109,12 @@
   ];
 
   # - Steam
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  # };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   # - Virtualisation
   virtualisation = {
