866eede1dd56d1d32af889fcd2c125bc3a469d9d
Vignesh Pai <57759856+vigneshpai2003@users.noreply.github.com>
2024-07-17 16:55:04 +0530

    declaratively manage themes removed nwg-look

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -1,13 +1,44 @@
 { pkgs, config, ... }:
-{
-  home.packages = with pkgs;[
-    dconf-editor # - GTK Settings
-    nwg-look # - GTK3 Settings
-    kdePackages.qt6ct # - Qt6 Theming
-    libsForQt5.qt5ct # - Qt5 Theming
-    kdePackages.qtstyleplugin-kvantum # - Qt6 Theming
-    libsForQt5.qtstyleplugin-kvantum # - Qt5 Theming
+let
+  theme = {
+    name = "adw-gtk3-dark";
+    package = pkgs.adw-gtk3;
+  };
 
-    adwaita-icon-theme
-  ];
+  cursorTheme = {
+    name = "Adwaita";
+    size = 24;
+    package = pkgs.adwaita-icon-theme;
+  };
+
+  iconTheme = {
+    name = "kora";
+    package = pkgs.kora-icon-theme;
+  };
+in
+{
+  home = {
+    packages = with pkgs;[
+      dconf-editor # - GTK Settings
+      kdePackages.qt6ct # - Qt6 Theming
+      libsForQt5.qt5ct # - Qt5 Theming
+      kdePackages.qtstyleplugin-kvantum # - Qt6 Theming
+      libsForQt5.qtstyleplugin-kvantum # - Qt5 Theming
+
+      theme.package
+      cursorTheme.package
+      iconTheme.package
+    ];
+
+    sessionVariables = {
+      XCURSOR_THEME = cursorTheme.name;
+      XCURSOR_SIZE = "${toString cursorTheme.size}";
+    };
+  };
+
+  gtk = {
+    inherit cursorTheme iconTheme;
+    theme.name = theme.name;
+    enable = true;
+  };
 }
